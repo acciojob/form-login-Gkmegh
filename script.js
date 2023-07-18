a74bb6de9d626a31a1e7d4b18f8898b95ed2802e
Gkmegh <118533859+Gkmegh@users.noreply.github.com>
2023-07-18 15:08:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
-    //Write your code here
+     let first = document.getElementsByTagName("input")[0].value;
+    
+    let last = document.getElementsByTagName("input")[1].value;
+    
+    alert(first +" "+ last);
 
 }
